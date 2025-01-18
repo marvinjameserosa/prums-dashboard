f946ef470a6bcaf6d3a7cb43be31d8ec0364be0f
Marvin James A. Erosa <104268552+marvinjameserosa@users.noreply.github.com>
2025-01-18 23:59:44 +0800

    created responsive template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 
+import { Dashboard } from "@/components/dashboard"
 
 export default function Home() {
   return (
     <main className="overflow-hidden text-lg text-center">
+      <Dashboard/>
     </main>
   );
 }
---
+++ src/components/dashboard.tsx
@@ -0,0 +1,42 @@
+import { Button } from "@/components/ui/button"
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuItem } from "@/components/ui/dropdown-menu"
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card"
+import { Badge } from "@/components/ui/badge"
+import { WQI } from "@/components/wqi"
+import { Sensors } from "@/components/sensors"
+export function Dashboard() {
+  return (
+    <div className="flex min-h-screen w-full">
+      <div className="flex flex-1 flex-col">
+        <header className="sticky top-0 z-30 flex h-16 shrink-0 items-center gap-4 border-b bg-background px-4 sm:h-20 sm:px-6">
+          <h1 className="text-lg font-semibold sm:text-xl">PRAMS</h1>
+          <div className="ml-auto flex items-center gap-4">
+            <div></div>
+          </div>
+        </header>
+        <main className="flex-1 overflow-y-auto p-4 sm:p-6">
+          <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            <WQI />
+            <Sensors />
+            
+          
+          </div>
+        </main>
+      </div>
+    </div>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
