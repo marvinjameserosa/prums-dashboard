f095f7801d71fc0fef7d0587ade4b7e155c22a05
Marvin James A. Erosa <104268552+marvinjameserosa@users.noreply.github.com>
2025-01-20 07:30:48 +0800

    change firebase config

--- src/app/firebase/config.tsx
+++ src/app/firebase/config.tsx
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
     
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_apiKey,
-  authDomain: process.env.NEXT_PUBLIC_authDomain,
+  apiKey: process.env.NEXT_apiKey,
+  authDomain: process.env.NEXT_authDomain,
   databaseURL:process.env.NEXT_PUBLIC_databaseURL,
-  projectId: process.env.NEXT_PUBLIC_projectId,
-  storageBucket: process.env.NEXT_PUBLIC_storageBucket,
-  messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
-  appId: process.env.NEXT_PUBLIC_appId,
+  projectId: process.env.NEXT_projectId,
+  storageBucket: process.env.NEXT_storageBucket,
+  messagingSenderId: process.env.NEXT_messagingSenderId,
+  appId: process.env.NEXT_appId,
 };
 
 const app = initializeApp(firebaseConfig);
